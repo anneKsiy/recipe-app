09640d8043c4197e8fbf6a32b9efebd9619b374a
Abdullah Adam <abuidreesadam@gmail.com>
2020-08-18 16:11:28 +0200

    Updates

---
+++ precache-manifest.feb5fae37c5101126b922b86951fb47d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20bb267d90ccf848365fbcecbb60ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63ce84001300a4943eb1",
+    "url": "/static/css/main.49a500cf.chunk.css"
+  },
+  {
+    "revision": "7e68185431ebe3504f25",
+    "url": "/static/js/2.224214a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.224214a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ce84001300a4943eb1",
+    "url": "/static/js/main.15501077.chunk.js"
+  },
+  {
+    "revision": "559ea9b268b7819359f3",
+    "url": "/static/js/runtime-main.786b8bbc.js"
+  }
+]);
